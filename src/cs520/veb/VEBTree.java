2bb0b51b0bbf3f5d15a57b4420076a67ec6a21ca
Walid Budgaga <wbudgaga@gmail.com>
2020-07-14 13:06:51 -0600

    Update VEBTree.java

--- src/cs520/veb/VEBTree.java
+++ src/cs520/veb/VEBTree.java
@@ -3,24 +3,24 @@ package cs520.veb;
 // Precondition: leftEnd >= 2
 
 public class VEBTree extends Heap{
-	private final int 	BASECASE_SIZE = 3; 	// base case constant you change as you like. 
-											// you can make it big as VEBtree size (in this case we will deal only with naive heap
+	private final int 	BASECASE_SIZE = 3; 	// Base case constant.  You can change it as you like. 
+							// You can make it big as VEBtree size. In this case we will deal only with naive heap
 	
-	private NaiveHeap	sideHeap; 			// the sideHeap is defined as NaiveHeap so we can use the methods there
+	private NaiveHeap	sideHeap; 		// the sideHeap is defined as NaiveHeap so we can use the methods there
 	private Heap 		heap[];
 	
 	public VEBTree(int left, int right){
 		super(left,right);
-		int subtreeSize 	= subtreeRange(); 									// size   of subtrees 
-		int numberOfSubtrees= rootDegree();   									// number of subtrees
-		sideHeap 			= new NaiveHeap(0,numberOfSubtrees);
-		heap 				= new Heap[numberOfSubtrees];						// the heap can be VEBTree or Naive heap(in case of base case)
+		int subtreeSize 	= subtreeRange(); 					// size   of subtrees 
+		int numberOfSubtrees	= rootDegree();   					// number of subtrees
+		sideHeap 		= new NaiveHeap(0,numberOfSubtrees);
+		heap 			= new Heap[numberOfSubtrees];				// the heap can be VEBTree or Naive heap(in case of base case)
 			
 		// creating of child heaps
 		for (int i = 0; i < numberOfSubtrees ; ++i){
-			int l 	= getLeftEnd() + i * subtreeSize; 							// calculate left range
+			int l 	= getLeftEnd() + i * subtreeSize; 				// calculate left range
 			int r 	= (i == numberOfSubtrees -1)? getRightEnd(): 
-						getLeftEnd() + (i+1) * subtreeSize - 1; 				// calculate right range
+				   getLeftEnd() + (i+1) * subtreeSize - 1; 			// calculate right range
 			heap[i] = isBaseCase(l,r)? new NaiveHeap(l, r): new VEBTree(l,r);	// the heap can be VEBTree or Naive heap
 		}
 	}
@@ -41,11 +41,11 @@ public class VEBTree extends Heap{
 	
 	@Override
 	public int deleteMin() {
-		int bucketIndex = subtreeBucket(findMin());
-		int nextMin 	= heap[bucketIndex].deleteMin();
+		int bucketIndex 	= subtreeBucket(findMin());
+		int nextMin 		= heap[bucketIndex].deleteMin();
 		
 		if (nextMin == -1){
-			int nextBucket = sideHeap.deleteMin();
+			int nextBucket 	= sideHeap.deleteMin();
 			if(nextBucket != -1)
 				nextMin = heap[nextBucket].findMin();
 		}
