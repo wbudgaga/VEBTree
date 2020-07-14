25c930a6299ba7c4d6272f1aeef4a48c79196e12
Walid Budgaga <wbudgaga@gmail.com>
2020-07-14 13:22:06 -0600

    Update VEBTest.java

--- src/cs520/veb/VEBTest.java
+++ src/cs520/veb/VEBTest.java
@@ -3,20 +3,18 @@ package cs520.veb;
 import java.util.Scanner;
 
 public class VEBTest {
-
 	public static void main(String args[]){
-	       Scanner Input = new Scanner(System.in);
+	       Scanner Input 		= new Scanner(System.in);
 	        int leftEnd, rightEnd;
 	        System.out.print("Left end of the tree's range:  ");
-	        leftEnd = Input.nextInt();
+	        leftEnd 		= Input.nextInt();
 	        System.out.println(leftEnd);
 	        System.out.print("right of the tree's range:  ");
 	   
-	        rightEnd = Input.nextInt();
+	        rightEnd 		= Input.nextInt();
 	        System.out.println(rightEnd);
 	        
-	        VEBTree heap = new VEBTree(leftEnd, rightEnd);
-	//        NaiveHeap NH = new NaiveHeap(leftEnd, rightEnd);
+	        VEBTree heap 		= new VEBTree(leftEnd, rightEnd);
 
 	        int choice; // menu option choice
 
@@ -35,13 +33,13 @@ public class VEBTest {
 	            
 	    
 	            System.out.print ("\n\n     Which? ");
-	            choice = Input.nextInt();
+	            choice 		= Input.nextInt();
 	            System.out.print ("\n");
 	            if (choice == 1){
 	                int newVal;
 
 	                System.out.print ("\n\nInteger to insert: ");
-	                newVal = Input.nextInt();
+	                newVal 		= Input.nextInt();
 	                System.out.println("\n" + newVal);
 	                if (heap.insert(newVal))
 	                    System.out.println("Successful");
